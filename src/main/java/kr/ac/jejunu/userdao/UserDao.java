de81fcf8a28bde060c30d6b3ebeea6ad084f9331
JaeKwanKim <jk79thor@icloud.com>
2016-04-22 16:35:14 +0900

    문제 1번

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -3,14 +3,34 @@ package kr.ac.jejunu.userdao;
 import java.sql.*;
 
 public class UserDao {
+    public long add(User user) throws ClassNotFoundException, SQLException {
+        //Driver Class Load
+        Class.forName("com.mysql.jdbc.Driver");
+        // Connection    접속정보는? localhost jeju id : jeju pw: jejupw
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false", "root", "");
+        // 쿼리만들고
+        String sql = "insert into test(name, password) values(?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        // 실행
+        preparedStatement.executeUpdate();
+        long id = getLastInsertId(connection);
+        //자원을 해지한다.
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
     public User get(Long id) throws ClassNotFoundException, SQLException {
         //데이터는어디에?   Mysql
         //Driver Class Load
         Class.forName("com.mysql.jdbc.Driver");
         // Connection    접속정보는? localhost jeju id : jeju pw: jejupw
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false", "root", "");
         // 쿼리만들고
-        PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+        String sql = "select * from test where id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setLong(1, id);
         // 실행
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -27,5 +47,19 @@ public class UserDao {
         connection.close();
 
         return user;
+    }
+
+    public long getLastInsertId(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("select last_insert_id()");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        long id = resultSet.getLong(1);
+
+        resultSet.close();
+        preparedStatement.close();
+
+        return id;
     }
 }
--- src/test/java/kr/ac/jejunu/userdao/UserDaoTest.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTest.java
@@ -8,6 +8,22 @@ import static org.junit.Assert.assertEquals;
 
 public class UserDaoTest {
     @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        String name = "김재관";
+        String password = "1234";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        long id = userDao.add(user);
+        System.out.println(id);
+        assertEquals(name, user.getName());
+        assertEquals(password, user.getPassword());
+    }
+    @Test
     public void get() throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
         Long id = 1L;
